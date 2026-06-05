e8960f8e3c0b9b7ef3f972ff345064d4dc571f75
ESTHER <esterufiteyesu@gmail.com>
2026-06-05 15:33:11 +0200

    Initial React project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello World!</h1>
+      <p>My first React project deployed on Vercel 🚀</p>
+    </div>
+  );
+}
+
+export default App;
